bb4f4cde7bad4e69be4e6543de55eca4b2bad198
travis moulton <travis.moulton002@gmail.com>
2020-12-06 14:30:48 -0500

    changed databases

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgres://omrurrfrlibrhd:8c355b817551d26dfb2e23e1b0e5685573e133bd405859534a3be5f07a4f4c49@ec2-3-224-97-209.compute-1.amazonaws.com:5432/dc6lphae81dd1b'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
