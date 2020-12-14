efee537d8bf4a9fa6b34169881e6123727bc1254
travis moulton <travis.moulton002@gmail.com>
2020-12-14 18:17:23 -0500

    configured worker to sync up with heroku

--- config.py
+++ config.py
@@ -2,13 +2,12 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-   
-    
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
     BROKER_URL = os.environ['REDIS_URL']
-    # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
+
+    # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
     # BROKER_URL = 'redis://localhost:6379/0'
     
