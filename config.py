03cf5df0f0928a78d37b759aa3f862114a420b7f
travis moulton <travis.moulton002@gmail.com>
2020-12-14 17:27:04 -0500

    updated requirements.txt

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
-    BROKER_URL = os.environ['BROKER_URL']
+    BROKER_URL = os.environ['CLOUDAMQP_URL']
     # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
     # BROKER_URL = 'redis://localhost:6379/0'
